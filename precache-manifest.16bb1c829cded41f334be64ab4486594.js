b493d1d5d63cdc8806026feaeb6077cc9a221e03
Roman Zabolotskyi <roman.zabolotskyi@gmail.com>
2020-07-19 23:30:39 +0300

    Updates

---
+++ precache-manifest.16bb1c829cded41f334be64ab4486594.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f4a800f115ca26e87c0c27b89cc163f7",
+    "url": "/sort-properties/index.html"
+  },
+  {
+    "revision": "4b876f8999b941e35cf6",
+    "url": "/sort-properties/static/css/main.1af3be2a.chunk.css"
+  },
+  {
+    "revision": "4e06db0754d9aee7a2bf",
+    "url": "/sort-properties/static/js/2.fee53afa.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/sort-properties/static/js/2.fee53afa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4b876f8999b941e35cf6",
+    "url": "/sort-properties/static/js/main.13c3be34.chunk.js"
+  },
+  {
+    "revision": "962319813b3e43f0e2d3",
+    "url": "/sort-properties/static/js/runtime-main.d057cac5.js"
+  }
+]);
